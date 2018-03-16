c21cf177b8f52fc8bd2c524b545be3cb65d8a18d
Furufuru <mail@furufuru1212.net>
2018-03-16 20:30:31 +0900

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,8 @@ const About = () => (
     <p>ブログだよ</p>
     <p><a href='https://github.com/furufuru'>Github @furufuru</a></p>
     <p><a href='https://twitter.com/furunfuru'>Twitter @furunfuru</a></p>
+    <p><a href='https://keybase.io/furufuru'>Keybase @furufuru</a></p>
+    <p><a href='https://steamcommunity.com/id/Furufuru/'>Steam</a></p>
   </div>
 )
 
